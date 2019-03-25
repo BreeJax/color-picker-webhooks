3628434022731d026b53ff2e81fceb128a67c97c
Bree Jackson <bjacksonsisterwolf@gmail.com>
2019-03-25 00:23:18 -0400

    Why the fuck is the RANDOM button the easy POS?

--- src/App.js
+++ src/App.js
@@ -14,11 +14,31 @@ class App extends Component {
       MainColor: `hsla(262, 100%, 90%, 0.75)`
     }
   }
+  random = () => {
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+    let h = getRandomInt(300);
+    let s = getRandomInt(100);
+    let l = getRandomInt(100);
+    let a = Math.random().toFixed(2);
+      this.setState({Hue: h, Saturation: s, Light: l, Alpha:a});
+    let updateEverChangingHSLA = `hsla(${this.state.Hue}, ${this.state.Saturation}%, ${this.state.Light}%, ${this.state.Alpha})`
+         this.setState({ MainColor: updateEverChangingHSLA })
+  }
     getHSLA = () => {
-      console.log("here");
       let everChangingHSLA = `hsla(${this.state.Hue}, ${this.state.Saturation}%, ${this.state.Light}%, ${this.state.Alpha})`
          this.setState({ MainColor: everChangingHSLA })
     }
+
+    getSaturation = () => {
+      let newSaturation = ""
+    }
+  // satInput.addEventListener('input', () => {
+  //   sat = satInput.value
+  //   console.log(getHSL())
+  //   colorChange()
+  // })
 
 
   render() {
@@ -26,7 +46,8 @@ class App extends Component {
       <div className="App">
         <h1>TASTE THE RAINBOW</h1>
         <MainColor MainColor={this.state.MainColor}/>
-        <h2 onClick={this.getHSLA}>  {this.state.MainColor} </h2>
+        <h2>  {this.state.MainColor} </h2>
+        <button onClick={this.random}>random!</button>
         <form>
           <p>HUE</p>
           <Slider title="Hue" min="1" max="300" value="261"/>
@@ -35,7 +56,7 @@ class App extends Component {
           <p>LIGHT</p>
           <Slider title="Light" min="1" max="100" value="77" />
           <p>ALPHA</p>
-          <Slider title="Alpha" min="0" max="1" value="0.3" special="alphaImage"/>
+          <Slider title="Alpha" min="0" max="1" value={this.state.Saturation} special="alphaImage"/>
         </form>
       </div>
     )
