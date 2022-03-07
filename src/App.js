582bb4ea7ee00168dc04c639e1b68851a37ff349
Bree Jackson <breejax@Brees-MacBook-Pro.local>
2022-03-06 22:53:48 -0500

    changed slider format, fixed code so that color would change on button click

--- src/App.js
+++ src/App.js
@@ -1,84 +1,109 @@
-import React, { Component } from "react"
-import "./App.css"
-import MainColor from "./Components/MainColor"
-import Slider from "./Components/Slider"
+import React, { Component } from "react";
+import "./App.css";
+import MainColor from "./Components/MainColor";
+import Slider from "./Components/Slider";
 
 const getRandomInt = (max) => {
- return Math.floor(Math.random() * Math.floor(max));
-}
-const h = getRandomInt(300);
-const s = getRandomInt(100);
-const l = getRandomInt(100);
-const a = Math.random().toFixed(2);
-
+  return Math.floor(Math.random() * Math.floor(max));
+};
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      Hue:262,
-      Saturation:100,
+      Hue: 262,
+      Saturation: 100,
       Light: 90,
-      Alpha:0.75,
-      MainColor: `hsla(262, 100%, 90%, 0.75)`
-    }
+      Alpha: 0.75,
+      MainColor: `hsla(262, 100%, 90%, 0.75)`,
+    };
   }
 
   random = () => {
-      this.setState({Hue: h, Saturation: s, Light: l, Alpha:a});
-      this.getHSLA()
-  }
-    getHSLA = () => {
-      let everChangingHSLA = `hsla(${this.state.Hue}, ${this.state.Saturation}%, ${this.state.Light}%, ${this.state.Alpha})`
-         this.setState({ MainColor: everChangingHSLA })
+    const h = getRandomInt(300);
+    const s = getRandomInt(100);
+    const l = getRandomInt(100);
+    const a = parseFloat(Math.random().toFixed(2));
 
-    }
+    this.setState({ Hue: h, Saturation: s, Light: l, Alpha: a });
+    console.log(this.state);
+    this.getHSLA();
+    // console.log(`Hue: ${this.state.Hue}, Saturation: ${this.state.Saturation}, Light: ${this.state.Light}, Alpha: ${this.state.Alpha}`);
+  };
+  getHSLA = () => {
+    let everChangingHSLA = `hsla(${this.state.Hue}, ${this.state.Saturation}%, ${this.state.Light}%, ${this.state.Alpha})`;
+    this.setState({ MainColor: everChangingHSLA });
+  };
 
   updateHue = (value) => {
-      this.setState({Hue: value})
-    this.getHSLA()
-  }
+    this.setState({ Hue: value });
+    this.getHSLA();
+  };
 
   updateSaturation = (value) => {
-      this.setState({Saturation: value})
-    this.getHSLA()
-
-  }
+    this.setState({ Saturation: value });
+    this.getHSLA();
+  };
 
   updateLight = (value) => {
-      this.setState({Light: value})
-    this.getHSLA()
-
-  }
+    this.setState({ Light: value });
+    this.getHSLA();
+  };
 
   updateAlpha = (value) => {
-      this.setState({Alpha: value})
-    this.getHSLA()
-
-  }
+    this.setState({ Alpha: value });
+    this.getHSLA();
+  };
 
   render() {
     return (
       <div className="App">
         <h1>TASTE THE RAINBOW</h1>
-        <MainColor MainColor={this.state.MainColor}/>
-        <h2>  {this.state.MainColor} </h2>
+        <MainColor MainColor={this.state.MainColor} />
+        <h2> {this.state.MainColor} </h2>
         <button onClick={this.random}>random!</button>
         <form>
           <p>HUE</p>
-          <Slider title="Hue" min="0" max="300" value={this.state.Hue} step="1" handleChange={(value) => this.updateHue(value) }/>
+          <Slider
+            title="Hue"
+            min="0"
+            max="300"
+            value={this.state.Hue}
+            step="1"
+            handleChange={(value) => this.updateHue(value)}
+          />
           <p>SATURATION</p>
-          <Slider title="Saturation" min="0" max="100" value={this.state.Saturation} step="1" handleChange={(value) => this.updateSaturation(value) }/>
+          <Slider
+            title="Saturation"
+            min="0"
+            max="100"
+            value={this.state.Saturation}
+            step="1"
+            handleChange={(value) => this.updateSaturation(value)}
+          />
           <p>LIGHT</p>
-          <Slider title="Light" min="0" max="100" value={this.state.Light} step="1" handleChange={(value) => this.updateLight(value) }/>
+          <Slider
+            title="Light"
+            min="0"
+            max="100"
+            value={this.state.Light}
+            step="1"
+            handleChange={(value) => this.updateLight(value)}
+          />
           <p>ALPHA</p>
-          <Slider title="Alpha" min="0" max="1" value={this.state.Alpha} step="0.01" special="alphaImage" handleChange={(value) => this.updateAlpha(value) }/>
+          <Slider
+            title="Alpha"
+            min="0"
+            max="1"
+            value={this.state.Alpha}
+            step="0.01"
+            special="alphaImage"
+            handleChange={(value) => this.updateAlpha(value)}
+          />
         </form>
       </div>
-    )
+    );
   }
 }
 
-
-
-export default App
+export default App;
--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -4,7 +4,13 @@ class Slider extends Component {
   render() {
     return <div className="Slider" className={this.props.special} >
         <div className={this.props.title}>
-            <input name={this.props.title} onChange={(e) => {(this.props.handleChange(e.target.value))}} type="range" min={this.props.min} max={this.props.max} value={this.props.value} step={this.props.step}/>
+            <input
+            name={this.props.title}
+            onChange={(e) => {(this.props.handleChange(e.target.value))}}
+            type="range" min={this.props.min}
+            max={this.props.max} 
+            value={this.props.value}
+            step={this.props.step}/>
             </div>
     </div>
   }
